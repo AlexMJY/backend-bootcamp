4880a7a50ddd867d08af260adb5fd07580641fbb
AlexMJY <jmoon0227@gmail.com>
2024-07-24 14:13:54 +0900

    세션 정보

--- intellij_workspace/security_jwt/src/main/java/com/aico/security_jwt/controller/MainController.java
+++ intellij_workspace/security_jwt/src/main/java/com/aico/security_jwt/controller/MainController.java
@@ -1,8 +1,14 @@
 package com.aico.security_jwt.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 @ResponseBody
@@ -10,6 +16,15 @@ public class MainController {
 
     @GetMapping("/")
     public String mainP() {
-        return "main controller";
+        // 세션 정보 검색
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        return "Main Controller || " + name + " || " + role;
     }
 }
